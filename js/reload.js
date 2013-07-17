6da9dfed105cbf29d10667ffd21b1b8b83c2dac3
Eric Bloch <eric.bloch@gmail.com>
2013-07-16 22:30:29 -0400

    synchronous, full reload working

--- js/reload.js
+++ js/reload.js
@@ -2,10 +2,11 @@ jQuery(document).ready(function($) {
 
     $('input.mws_reload_posts').on('click', function(e) {
         url = window.wms_reload.url;
-        $.post(url, "", function(d, ts, jqx) {
+        $.post(url, {
+            action : 'reload_all'
+        }, function(d, ts, jqx) {
             alert(d);
         });
-
     });
 
 });
