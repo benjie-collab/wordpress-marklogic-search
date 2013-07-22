8e3f9b353b6b9634b99a1a57a09e917b0d94f4dd
Eric Bloch <eric.bloch@gmail.com>
2013-07-22 14:28:27 -0400

    Cleanup and fix the ajax stuff

--- js/clear.js
+++ js/clear.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     $('input.mws_clear').on('click', function(e) {
         url = window.wms_reload.url;
         $.post(url, {
-            action : 'clear'
+            action : 'wms_clear'
         }, function(d, ts, jqx) {
             alert(d);
         });
--- js/reload.js
+++ js/reload.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     $('input.mws_reload_posts').on('click', function(e) {
         url = window.wms_reload.url;
         $.post(url, {
-            action : 'reload_all'
+            action : 'wms_reload_all'
         }, function(d, ts, jqx) {
             alert(d);
         });
